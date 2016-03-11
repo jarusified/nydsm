4bea6270e63297c4481175de6743158ca1cca36d
suraj kesavan <jarus@surajs-macbook-pro.local>
2016-03-11 15:24:12 +0530

    Initial Commit

---
+++ nydsm/nydsmView.m
@@ -0,0 +1,52 @@
+//
+//  nydsmView.m
+//  nydsm
+//
+//  Created by suraj kesavan on 11/03/16.
+//  Copyright © 2016 suraj kesavan. All rights reserved.
+//
+
+#import "nydsmView.h"
+
+@implementation nydsmView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
