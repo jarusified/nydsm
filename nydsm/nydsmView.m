3fea753de4d213fde374ad4cc4786a40bfb54874
suraj kesavan <jarus@surajs-MacBook-Pro.local>
2016-03-11 21:34:09 +0530

    basic rect, curve rendering

--- nydsm/nydsmView.m
+++ nydsm/nydsmView.m
@@ -36,6 +36,49 @@
 
 - (void)animateOneFrame
 {
+    NSBezierPath *path;
+    NSRect rect;
+    NSSize size;
+    NSColor *color;
+    float red, green, blue, alpha;
+    int shapeType;
+    
+    size = [self bounds].size;
+    rect.size = NSMakeSize(SSRandomFloatBetween(size.width/100.0, size.width/100.0), SSRandomFloatBetween(size.height/100.0, size.height/100.0));
+    rect.origin = SSRandomPointForSizeWithinRect(rect.size, [self bounds]);
+    shapeType = SSRandomIntBetween(0, 2);
+    
+    switch (shapeType) {
+        case 0:
+            path = [NSBezierPath bezierPathWithRect:rect];
+            break;
+            
+        case 1:
+            path = [NSBezierPath bezierPathWithOvalInRect:rect];
+            break;
+            
+        default:{
+            float startAngle, endAngle, radius;
+            NSPoint point;
+            startAngle = SSRandomFloatBetween(0.0, 360.0);
+            endAngle = SSRandomFloatBetween(startAngle, 360.0 + startAngle);
+            radius = rect.size.width <= rect.size.height ? rect.size.width/2 : rect.size.height/2;
+            point = NSMakePoint(rect.origin.x + rect.size.width/2 , rect.origin.y + rect.size.height/2);
+            path = [NSBezierPath bezierPath];
+            [path appendBezierPathWithArcWithCenter:point radius:radius startAngle:startAngle endAngle:endAngle clockwise:SSRandomIntBetween(0, 1)];
+            break;
+        }
+    }
+    
+    red = SSRandomFloatBetween(0.0, 255.0)/255.0;
+    green = SSRandomFloatBetween(0.0, 255.0)/255.0;
+    blue = SSRandomFloatBetween(0.0, 255.0)/255.0;
+    alpha = SSRandomFloatBetween(0.0, 255.0)/255.0;
+    
+    color = [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:alpha];
+    [color set];
+    [path stroke];
+   
     return;
 }
 
